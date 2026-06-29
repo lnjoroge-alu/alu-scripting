144173a6c43e9d20ab20215f02e6afcede82fbbc
lnjoroge-alu <l.njoroge@alustudent.com>
2026-06-29 22:56:16 +0300

    fix: add try block

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -17,10 +17,13 @@ def number_of_subscribers(subreddit):
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
+    response = get(url, headers=user_agent, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
 
     try:
+        results = response.json()
         return results.get('data').get('subscribers')
 
     except Exception:
--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -17,15 +17,21 @@ def top_ten(subreddit):
 
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
+        return
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
+    response = get(url, headers=user_agent, params=params,
+                   allow_redirects=False)
+
+    if response.status_code != 200:
+        print("None")
+        return
 
     try:
+        results = response.json()
         my_data = results.get('data').get('children')
 
         for i in my_data:
